36c517e25e697ef4db900cb9fda8a1170f1de50d
Trong Dang <trong_1751220006@dau.edu.vn>
2021-09-28 15:49:13 +0700

    get_api

--- src/App.js
+++ src/App.js
@@ -2,29 +2,41 @@ import TodoFeature from './features/Todo';
 import AlbumFeature from './features/Album';
 import NotFound from './components/NotFound';
 import { Switch, NavLink, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import productApi from './api/productApi';
 
 function App() {
-  return (
-    <div className="App">
-      Header
-      <p>
-        <NavLink to="todos" activeClassName="active-menu">
-          Todos
-        </NavLink>
-      </p>
-      <p>
-        <NavLink to="albums" activeClassName="active-menu">
-          Albums
-        </NavLink>
-      </p>
-      <Switch>
-        <Route path="/todos" component={TodoFeature} />
-        <Route path="/albums" component={AlbumFeature} />
-        <Route component={NotFound} />
-      </Switch>
-      Footer
-    </div>
-  );
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const params = {
+                _limit: 10,
+            };
+            const productList = await productApi.getAll(params);
+            console.log(productList);
+        };
+        fetchProducts();
+    }, []);
+    return (
+        <div className="App">
+            Header
+            <p>
+                <NavLink to="todos" activeClassName="active-menu">
+                    Todos
+                </NavLink>
+            </p>
+            <p>
+                <NavLink to="albums" activeClassName="active-menu">
+                    Albums
+                </NavLink>
+            </p>
+            <Switch>
+                <Route path="/todos" component={TodoFeature} />
+                <Route path="/albums" component={AlbumFeature} />
+                <Route component={NotFound} />
+            </Switch>
+            Footer
+        </div>
+    );
 }
 
 export default App;
